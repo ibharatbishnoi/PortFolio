29d661b2270672c0aff5507fa6bd87a4107b54c8
Bharat Bishnoi <159180207+ibharatbishnoi@users.noreply.github.com>
2025-07-29 13:04:08 +0530

    Update Education.tsx

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -17,25 +17,25 @@ const Education = () => {
           <ResumeCard
             title="B.Tech in Computer Science"
             subTitle="Vellore Institute of Technology (2021-2025)"
-            result="CGPA 8.11"
+            result="CGPA 8.78"
             des="Currently pursuing my B.Tech in Computer Science at Vellore Institute of Technology, with an expected graduation in 2025. Gaining hands-on experience in algorithms, data structures, and software engineering, I'm passionate about contributing innovative solutions to the tech industry."
           />
 
           <ResumeCard
             title="Higher Secondary Education"
-            subTitle="Chhatrapati Shivaji Sr Sec Sch (2018-2020)"
-            result="Percentage 66%"
+            subTitle="Chhatrapati Shivaji Sr Sec Sch, Kota(2018-2020)"
+            result=" "
             des="I completed my higher secondary education at Chhatrapati Shivaji Sr Sec Sch, laying the groundwork for my current pursuit of a B.Tech in Computer Science."
           />
 
           {/* Uncomment and complete if you want to include this section */}
-          {/* <ResumeCard
+          * <ResumeCard
             title="Secondary School Education"
-            subTitle="St. Paul's School, Darjeeling (2016 - 2018)"
-            result="83.8/100"
-            des="I successfully completed my secondary education at St. Paul's School in May 2018.
+            subTitle="Cambridge Public School, Jodhpur (2016 - 2018)"
+            result=" "
+            des="I successfully completed my secondary education at Cambridge Public School in May 2018.
             This period marked the beginning of my academic journey and laid the groundwork for my subsequent educational pursuits."
-          /> */}
+          /> *
         </div>
       </div>
     </motion.div>
