959480026b2b3cbc80c03a8bf09f74861545ffa5
Bharat Bishnoi <159180207+ibharatbishnoi@users.noreply.github.com>
2025-07-30 11:59:12 +0530

    Update RightBanner.tsx

--- src/components/RightBanner.tsx
+++ src/components/RightBanner.tsx
@@ -5,11 +5,11 @@ const RightBanner = () => {
   return (
     <FadeIn className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10 rounded-xl object-cover"
         src={bannerImg}
         alt="bannerImg"
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#0B1120] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-white to-gray-100 shadow-xl rounded-xl"></div>
     </FadeIn>
   );
 };
